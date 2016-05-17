a6e2b53115e307f04ebad246864fb5476f202a01
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-05-17 16:38:00 +0530

    Added some helper functions

--- helpers/mark_spam.py
+++ helpers/mark_spam.py
@@ -4,30 +4,31 @@ import Image
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.analysis
-table = db.tags
-table2 = db.tags_with_spam
+table = db.tags_with_spam
 
 path = os.path.expanduser("~/Desktop/Images5K")
 
 tbp = table.find()
 
+count = 1
+
 for row in tbp:
 	entry = row
 	idee = entry['_id']
 	del entry['_id']
+	del entry['spam']
+	print entry['filename']
 	img = Image.open(path + "/" + entry['filename'])
 	img.show()
 	# cv2.waitKey()
-	print "T: ",entry["tensorflow_tag"],"(",str(entry["tensorflow_confidence"]),")"
-	print "D: ",entry["densecap_tag"],"(",str(entry["densecap_confidence"]),")"
-	x,y,z = input()
+	x = input("Spam : ")
 	if x == -1:
 		print "Exiting"
 		exit()
-	entry["tensorflow_correct"] = x
-	entry["densecap_correct"] = y
-	entry["spam"] = z
-	# Insert to new DB
-	table2.insert_one(entry)
+	entry["spam"] = x
 	# Delete from old DB
 	table.delete_one({'_id': idee})
+	# Insert to new DB
+	table.insert_one(entry)
+	print count
+	count += 1
