490779d9b30fb3dd3a1c37b4a5b05d20938a2d85
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-07-14 09:14:22 +0530

    Converting Varun's code to a callable module. Sys calls are not cool :p

--- precog_testing/__init__.py
+++
@@ -1,1 +0,0 @@
-import classify_image
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 
-setup(name='precog_testing',
+setup(name='image_analyse',
       version='0.1',
       description='PreCog stuff',
       url='https://github.com/iamgroot42/image-classification',
@@ -8,7 +8,7 @@ setup(name='precog_testing',
       author_email='anshuman14021@iiitd.ac.in',
       license='MIT',
       packages=find_packages(),
-      package_data={'precog_testing': ['NN_Data/*']},
+      package_data={'image_analyse': ['NN_Data/*']},
       install_requires=[
           'pymongo',
           'numpy',
