022a87cc230056d8f82255f4d13a9156a812bcb4
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-07-17 11:34:51 +0530

    Server now works with url of image, in addition to POSTed image. Basic extension backbone added.

--- image_analyse/server.py
+++ image_analyse/server.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 from PIL import Image
 from scipy import misc
 import StringIO
+import urllib2
 
 app = Flask(__name__)
 
@@ -13,10 +14,26 @@ reader = None
 predictor = None
 
 
-@app.route("/analyze",  methods=['POST'])
-def analyze():
+@app.route("/analyze_image",  methods=['POST'])
+def analyze_image():
 	imagefile = request.files['imagefile']
 	img =  imagefile.stream.read()
+	try:
+		ret_json = {}
+		img_array = misc.fromimage(Image.open(StringIO.StringIO(img)))
+		ret_json['sentiment'] = face.get_sentiment(clf, reader, predictor, img, img_array)
+		ret_json['tag'] = tag.tensor_inference(img)
+		return str(ret_json)
+	except Exception, e:
+		print e
+	return "Could not process image"
+
+
+@app.route("/analyze_url",  methods=['GET'])
+def analyze_url():
+	image_url = request.args['image_url']
+	response = urllib2.urlopen(image_url)
+	img = response.read()
 	try:
 		ret_json = {}
 		img_array = misc.fromimage(Image.open(StringIO.StringIO(img)))
