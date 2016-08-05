cff59c4750dbea1ec374d504423e2b3b9de8dc29
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-08-05 23:38:10 +0530

    Apart from icons, access token, ergonomics, extension is ready :)

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -45,7 +45,7 @@ $ (document).ready( function() {
               image_id = url.split("/")[url.split("/").length-2];
             }
             // var access_token = "<ACCESS TOKEN FROM APP/IPOD>";
-            var access_token = "EAACEdEose0cBAKfccPT5DMRmpb4NmQGatD1mTsbsio3IWKWE3Rah5O8p9XEthBMDYJ6hgVWkIZBxt3GVpL1mFjKu4GukBf0uZB7kfN3BEK09xkYQAECDAfKR3vKiEZB1re4qPHTYGsNZCOkPPwbzgO7wgrt93LfwavcmWYUCFQZDZD";
+            var access_token = "EAACEdEose0cBAMhBYEwF9UfdZBI0m5J2e01EBZA7JkmPNQ9tTGQ5UNGL6ZCaZCOGOj8Q4EZBLafYmzGnHmScyOw1lVc3lToSeevGHEWwa0eAUSmeHchM6jLuOeHUTHUXIQXMeIzTzIfsPHhYyd97zjFJFb3pgQ7uI7lSd9PPtSgZDZD";
             var graph_url = "https://graph.facebook.com/v2.3/" + image_id + "?fields=source&access_token=" + access_token;
             var apiCallUrl =  "http://labs.precog.iiitd.edu.in/resources/HelixAPI/analyze_url?image_url=";
             // Way to get send message compatibility over all chrome browser versions
@@ -59,9 +59,9 @@ $ (document).ready( function() {
                     if('source' in graph_obj){
                       var image_url = graph_obj['source'];
                       // Convert png to jpg (if it is png) by simply renaming; FB allows that :)
-                      // if(image_url.lastIndexOf(".png") == image_url.length - 3){
-                      // 	image_url = image_url.split(".png")[0] + ".jpg";
-                      // }
+                      if(image_url.lastIndexOf(".png") == image_url.length - 3){
+                      	image_url = image_url.split(".png")[0] + ".jpg";
+                      }
                       if (!chrome.runtime) {
                         // Chrome 20-21
                         chrome.runtime = chrome.extension;
@@ -82,29 +82,59 @@ $ (document).ready( function() {
                           $(elem).find(".HelixAPIanalysis").html("");
                           var obj = JSON.parse(responseText);
                           if(!("error" in obj)){
-                          	console.log(responseText);
                           	var senti = obj['sentiment'];
                           	var tag = obj['tag'];
                           	var text = obj['text'];
-                          	if(text != ""){
-                          		// Display 'text'
-                          	}
-                          	for (var key in senti['faces']) {
-							  	// Insert one of 5 emoticons depending on value of 
-							  	// senti['faces'][key]['Positive']
-							}
-							// Insert one of 5 emoticons depending on value of 
-							// senti['Tensorflow_SentiBank']['Positive']
-							try{
-								// Insert one of 5 emoticons depending on value of 
-								// senti['Average']['Positive']
-							}
-							catch(err){
-								// Do nothing, no faces, so no emotion
-							}
-							// Display tag, with color in [R,G] proportional to senti['tag']['confidence']
-							// senti['tag']['tag']
-                          }
+                            var inner_html = "";
+                            // Inceptionv3 tag
+                            inner_html += "<b>" + tag['tag'] + "</b> (" + Math.round(100*tag['confidence']) + "% confidence)<br>";
+                            // Sentibank model sentiment
+                            if(senti["Tensorflow_SentiBank"]["Positive"] > senti["Tensorflow_SentiBank"]["Negative"])
+                            {
+                                inner_html += "<b>Positive sentiment</b> " + " (" + Math.round(100*senti["Tensorflow_SentiBank"]["Positive"]) + "% confidence)<br>";
+                            }
+                            else
+                            {
+                                inner_html += "<b>Negative sentiment</b> " + " (" + Math.round(100*senti["Tensorflow_SentiBank"]["Negative"]) + "% confidence)<br>";
+                            }
+                            // Text,if any
+                            if(text != ""){
+                                inner_html += "<b>Text in image:</b> " + text + "<br>";
+                            }
+                            var i = 1;
+                            // Sentiment per face
+                            for (var key in senti['Face']) {
+                                inner_html += "<b>Face " + i.toString() + ": ";
+                                if(senti["Face"][key]["Positive"] > senti["Face"][key]["Negative"])
+                                {
+                                    inner_html += "Positive sentiment</b> " + " (" + Math.round(100*senti["Face"][key]["Positive"]) + "% confidence)<br>";
+                                }
+                                else
+                                {
+                                    inner_html += "Negative sentiment</b> " + " (" + Math.round(100*senti["Face"][key]["Negative"]) + "% confidence)<br>";
+                                }
+                                i++;
+                             }
+                             if(senti['Face']['Average']){
+
+                                inner_html += "<b>Average sentiment (from faces):</b> ";
+                                if(senti['Face']['Average']['Positive'] > senti['Face']['Average']["Negative"])
+                                {
+                                    inner_html += "Positive " + " (" + Math.round(100*senti['Face']['Average']['Positive']) + "% confidence)<br>";
+                                }
+                                else
+                                {
+                                    inner_html += "Negative " + " (" + Math.round(100*senti['Face']['Average']["Negative"]) + "% confidence)<br>";
+                                }
+                             }
+                             var add_button = document.createElement("button");
+                             add_button.innerHTML = "Potato";
+                             add_button.onclick = function(){
+                                // Sweet alert
+                                swal({title: "<small>Helix\'s analysis</small>", text: inner_html, html: true }); 
+                            };
+                            elem.appendChild(add_button);
+                            }
                         });
                       }
                   }
